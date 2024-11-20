8303349a49ac2ef32e14ae7864e145c61ade0d4c
Ian Wekesa Wafula <94100567+wequeteche@users.noreply.github.com>
2024-11-20 19:22:08 +0300

    Update settings.py
    Enabled hosting on all platforms '*'

--- cecilshop/cecilshop/settings.py
+++ cecilshop/cecilshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-e2edpq@la#on7y!2^guqsh+q-fbqrt9rie7l@ico2h(*n+j&yf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
